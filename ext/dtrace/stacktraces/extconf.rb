a66caa11ba66b2e30054ae01ef85ae3ce9524652
Eric Saxby <sax@livinginthepast.org>
2015-08-28 17:02:24 -0700

    ext actually compiles dtrace helper

--- ext/dtrace/stacktraces/extconf.rb
+++ ext/dtrace/stacktraces/extconf.rb
@@ -4,13 +4,17 @@ def development?
   !!ENV['OLDPWD']
 end
 
-if have_library("dtrace", "dtrace_open", "/usr/include/dtrace.h")
-  system('cp $OLDPWD/ruby_dtrace_ustack.d .') if development?
+if have_library('dtrace', 'dtrace_open', '/usr/include/dtrace.h')
+  # system('cp $OLDPWD/ruby_dtrace_ustack.d .') if development?
 
-  system('dtrace -h -s ruby_dtrace_ustack.d')
-  system('dtrace -64 -C -G -s ruby_dtrace_ustack.d')
+  d_script = File.expand_path('../ruby_dtrace_ustack.d', __FILE__)
+  d_header = File.expand_path('../ruby_dtrace_ustack.h', __FILE__)
 
-  system('cp ruby_dtrace_ustack.{h,o} $OLDPWD') if development?
+  system('/usr/sbin/dtrace -h -s %s' % d_script)
+  # abort 'Unable to create headers from dtrace ustack helper' unless have_header(d_header)
+  system('/usr/sbin/dtrace -64 -C -G -s %s' % d_script)
+
+  # system('cp ruby_dtrace_ustack.{h,o} $OLDPWD') if development?
   create_makefile('ruby_ustack')
 end
 
