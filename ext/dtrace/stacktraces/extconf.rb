dcc0da1153e09b97c27cada1283badd061c2490d
Eric Saxby <sax@livinginthepast.org>
2015-09-15 10:54:44 -0700

    Abort compilation if unable to compile dtrace

--- ext/dtrace/stacktraces/extconf.rb
+++ ext/dtrace/stacktraces/extconf.rb
@@ -10,9 +10,9 @@ if have_library('dtrace', 'dtrace_open', '/usr/include/dtrace.h')
   d_script = File.expand_path('../ruby_dtrace_ustack.d', __FILE__)
   d_header = File.expand_path('../ruby_dtrace_ustack.h', __FILE__)
 
-  system('/usr/sbin/dtrace -h -s %s' % d_script)
+  abort 'Unable to export dtrace headers' unless system('/usr/sbin/dtrace -h -s %s' % d_script)
   # abort 'Unable to create headers from dtrace ustack helper' unless have_header(d_header)
-  system('/usr/sbin/dtrace -64 -C -G -s %s' % d_script)
+  abort 'Unable to compile dtrace script' unless system('/usr/sbin/dtrace -64 -C -G -s %s' % d_script)
 
   # system('cp ruby_dtrace_ustack.{h,o} $OLDPWD') if development?
   create_makefile('ruby_ustack')
